3297729b6d3b46078bbacfc8ac6c9ae13af0a56b
Horii <hjk050111@gmail.com>
2025-11-27 10:46:04 +0900

    version3_d

--- Chapter02/Branch01.java
+++ Chapter02/Branch01.java
@@ -3,7 +3,9 @@ pakage Chapter02;
 public class Branch01 {
 
     public Branch01() {
-        System.out.printIn("메인 브렌치에서 수정");
+        System.out.printIn("메인 브렌치에서 작업");
+
+        System.out.printIn("다음 브렌치에서 작업");
     }
 
 }
