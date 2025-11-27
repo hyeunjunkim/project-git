ba279d32e9cb51cb035a3dd613caf141761685fc
Horii <hjk050111@gmail.com>
2025-11-27 10:55:31 +0900

    version4_g

--- Chapter02/Branch01.java
+++ Chapter02/Branch01.java
@@ -3,7 +3,7 @@ pakage Chapter02;
 public class Branch01 {
 
     public Branch01() {
-        System.out.printIn("메인 브렌치에서 작업");
+        System.out.printIn("구글 브렌치에서 작업");
 
         System.out.printIn("다음 브렌치에서 작업");
     }
